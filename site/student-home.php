1013d5a4edafacdb9b6e3ef416b464da1bd28328
Keshav Agarwal <99586399+agkeshav@users.noreply.github.com>
2023-10-31 00:59:09 +0530

    Student Dashboard frontend done

--- site/student-home.php
+++ site/student-home.php
@@ -1,5 +1,5 @@
 <?php
-  session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -10,165 +10,164 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Student</title>
-
-  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet" />
+  <link rel="stylesheet" href="student-home.css">
   <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
-
   <link href="https://fonts.googleapis.com/css?family=Be+Vietnam:400,600,800&display=swap" rel="stylesheet">
 </head>
 
-<body>
 
-<?php
+<body class="dark-theme">
+
+  <?php
   $servname = "localhost";
   $conn = new mysqli($servname, "root", "", "college_db");
 
   if ($conn->connect_error)
     die("Connection failed: " . $conn->connect_error);
-?>
-
-<div class="page-container">
-
-<div class="user-label rect-circ">
-  <span class="rect-circ">STUDENT</span>
-</div>
-
-<div class="btn-logout rect-circ" onClick="logout()">
-  <span class="rect-circ">LOGOUT</span>
-  <div class="rect-circ">✖</div>
-</div>
-
-<div class="user-info">
-  <?php
-    $sql = "SELECT StudentID, PersonID FROM STUDENT
-            WHERE STUDENT.StudentID = ".$_SESSION["userid"];
-    $res = $conn->query($sql);
-
-    $pid = -1;
-    if ($res->num_rows > 0) {
-      while ($row = $res->fetch_assoc()) {
-        $pid = $row['PersonID'];
-      }
-    }
-
-    $sql = "SELECT * FROM PERSON WHERE PERSON.PersonID = ".$pid;
-    $res = $conn->query($sql);
-
-    if ($res->num_rows > 0) {
-      while ($row = $res->fetch_assoc()) {
-        $name = $row['FirstName']." ";
-        if ($row['MiddleName'] != "")
-          $name .= $row['MiddleName']." ";
-        $name .= $row['LastName'];
-      }
-    }
-
-    echo "<span>$name</span>";
-    echo "<span>"; echo $_SESSION["userid"]; echo "</span>";
   ?>
-</div>
+  <header>
+    <div class="navbar">
+      <div onClick="logout()">
+        <span class="material-icons-sharp" onClick="logout()"></span>
+        <h3>Logout</h3>
+      </div>
 
-<?php
-  $courses = array();
+    </div>
+  </header>
+  <div class="container" style="display: flex; flex-direction: column;  ">
+    <div class="profile">
+      <div class="top">
+        <div class="info" style="text-align: center;">
+          <?php
+          $sql = "SELECT StudentID, PersonID FROM STUDENT
+            WHERE STUDENT.StudentID = " . $_SESSION["userid"];
+          $res = $conn->query($sql);
 
-  $sql = "SELECT * FROM UNDERTAKES, COURSE, DEPARTMENT
-          WHERE UNDERTAKES.StudentID =".$_SESSION["userid"].
-          " AND UNDERTAKES.CourseID = COURSE.CourseID
+          $pid = -1;
+          if ($res->num_rows > 0) {
+            while ($row = $res->fetch_assoc()) {
+              $pid = $row['PersonID'];
+            }
+          }
+
+          $sql = "SELECT * FROM PERSON WHERE PERSON.PersonID = " . $pid;
+          $res = $conn->query($sql);
+
+          if ($res->num_rows > 0) {
+            while ($row = $res->fetch_assoc()) {
+              $name = $row['FirstName'] . " ";
+              if ($row['MiddleName'] != "")
+                $name .= $row['MiddleName'] . " ";
+              $name .= $row['LastName'];
+            }
+          }
+          echo "<p style='font-size: 24px;'>Hey, <b style='font-size: 28px;'>$name</b></p>";
+          echo '<small style="font-size: 18px;"> ID :- ';
+          echo $_SESSION["userid"];
+          echo "</small>";
+          ?>
+        </div>
+      </div>
+    </div>
+    <?php
+    $courses = array();
+
+    $sql = "SELECT * FROM UNDERTAKES, COURSE, DEPARTMENT
+          WHERE UNDERTAKES.StudentID =" . $_SESSION["userid"] .
+      " AND UNDERTAKES.CourseID = COURSE.CourseID
           AND DEPARTMENT.DeptNo = COURSE.DeptNo";
-  $res = $conn->query($sql);
-  
-  if ($res->num_rows > 0) {
-    while ($row = $res->fetch_assoc()) {
-      array_push($courses,
-        array($row['DeptName'], $row['CourseID'], $row['CourseName'],
-          $row['ClassesTaken'], $row['Attendance'], $row['InternalMarks'], $row['PaperMarks']));
+    $res = $conn->query($sql);
+
+    if ($res->num_rows > 0) {
+      while ($row = $res->fetch_assoc()) {
+        array_push(
+          $courses,
+          array(
+            $row['DeptName'],
+            $row['CourseID'],
+            $row['CourseName'],
+            $row['ClassesTaken'],
+            $row['Attendance'],
+            $row['InternalMarks'],
+            $row['PaperMarks']
+          )
+        );
+      }
     }
-  }
-
-
-  for ($i = 0; $i < count($courses); $i++) {
-    $total = $courses[$i][5] + $courses[$i][6];
-    $grdpt = min(floor($total / 10) + 1, 10);
+    echo '<h1 style="display:inline;">Attendance</h1>';
+    echo '<main style="display: flex; flex-direction: row;">
+      ';
+    for ($i = 0; $i < count($courses); $i++) {
+      $total = $courses[$i][5] + $courses[$i][6];
+      $grdpt = min(floor($total / 10) + 1, 10);
 
       if (intval($courses[$i][3]) > 0) {
-    $attendancePercentage = round(intval($courses[$i][4]) / intval($courses[$i][3]) * 100);
-} else {
-    $attendancePercentage = 0; // Set to 0 if the denominator is 0 to avoid division by zero.
-}
-
-    echo
-    '<div class="rect-round-sm std-course-container">
-      <div class="std-course-info">
-        <div class="rect-round-sm std-course-dept">
-          <span>'.$courses[$i][0].'</span>
+        $attendancePercentage = round(intval($courses[$i][4]) / intval($courses[$i][3]) * 100);
+      } else {
+        $attendancePercentage = 0; // Set to 0 if the denominator is 0 to avoid division by zero.
+      }
+      echo '
+      <div class="subjects" style=" margin-left:20px">
+        <div class="eg">
+          <span class="material-icons-sharp">architecture</span>
+          <h2>' . $courses[$i][0] . '</h2>
+           <h2>' . $courses[$i][1] . ' :- ' . $courses[$i][2] . '</h2>
+          <h2>' . $courses[$i][4] . '/' . $courses[$i][3] . '</h2>
+          <div class="progress">
+            <svg>
+              <circle cx="38" cy="38" r="36"></circle>
+            </svg>
+            <div class="number">
+              <p>' . $attendancePercentage . '%</p>
+            </div>
+          </div>  
         </div>
-      
-        <div class="rect-round-sm std-course-cred">
-          <div class="rect-round-sm std-course-id">
-            <span>'.$courses[$i][1].'</span>
-          </div>
-
-          <div class="std-course-name">
-            <span>'.$courses[$i][2].'</span>
-          </div>
-        </div>
+        
       </div>
+    ';
 
-      <div class="std-course-data">
-        <div class="std-course-attendance">
-          <div class="std-course-attendance-data">
-            <span>Total Classes: </span>
-            <span>'.$courses[$i][3].'</span>
-          </div>
-  
-          <div class="std-course-attendance-data">
-            <span>Classes Attended: </span>
-            <span>'.$courses[$i][4].'</span>
-          </div>
+    }
+    echo '</main>';
+    echo '<h1>Marks</h1>';
+    echo '<main style="display: flex; flex-direction: row;">
+';
+    for ($i = 0; $i < count($courses); $i++) {
+      $total = $courses[$i][5] + $courses[$i][6];
+      $grdpt = min(floor($total / 10) + 1, 10);
 
 
-          <div class="std-course-attendance-data">
-            <span>Attendance: </span>
-            <span>'.$attendancePercentage.'%</span>
-          </div>
+      echo '
+      <div class="subjects" style="margin-top:35px; margin-left:20px">
+        <div class="eg">
+          <span class="material-icons-sharp">architecture</span>
+          <h2>' . $courses[$i][0] . '</h2>
+           <h2>' . $courses[$i][1] . ' :- ' . $courses[$i][2] . '</h2>
+          <h3>Internal Marks:- ' . $courses[$i][5] . '</h3>
+          <h3>Paper Marks:- ' . $courses[$i][6] . '</h3>
+          <h3>Total Marks:- ' . $total . '</h3>
+          <h3>Grade Point:- ' . $grdpt . '</h3>
+          
         </div>
-  
-        <div class="std-course-marks">
-          <div class="std-course-marks-data">
-            <span>Internal Marks: </span>
-            <span>'.$courses[$i][5].'</span>
-          </div>
-  
-          <div class="std-course-marks-data">
-            <span>Paper Marks: </span>
-            <span>'.$courses[$i][6].'</span>
-          </div>
-  
-          <div class="std-course-marks-data">
-            <span>Total: </span>
-            <span>'.$total.'</span>
-          </div>
-  
-          <div class="std-course-marks-data">
-            <span>Grade Point: </span>
-            <span>'.$grdpt.'</span>
-          </div>
-        </div>
+        
       </div>
-    </div>';
-  }
-?>
+    ';
 
-</div>
+    }
+    echo '</main>';
 
-<script>
-  function logout() {
-    document.cookie = "courseid" + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.cookie = "loggedin" + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.cookie = "logout=yes";
-    window.location.href = 'index.php';
-  }
-</script>
+    ?>
+
+  </div>
+
+  <script>
+    function logout() {
+      document.cookie = "courseid" + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+      document.cookie = "loggedin" + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+      document.cookie = "logout=yes";
+      window.location.href = 'index.php';
+    }
+  </script>
 
 </body>
 
